4e36c39877b0a213b3587ac7f053405ae9ffac25
alumno <alumno@aula1-02>
2019-11-13 14:37:28 +0100

    Sección esFuerte corregida

--- Password_Objetos/src/principal/Password.java
+++ Password_Objetos/src/principal/Password.java
@@ -5,18 +5,63 @@ import java.util.Random;
 public class Password {
 	private int longitud;
 	private String contraseña;
+	
 	public Password() {
-		longitud = 0;
-		contraseña = "";
+		longitud = 8;
+		generarPassword(longitud);
 	}
 	public Password(int longitud) {
 		this.longitud = longitud;
-		contraseña = generarPassword();
+		generarPassword(longitud);
 	}
 	public boolean esFuerte() {
-		return true;
+		int Mayusculas = 0;
+		int Minusculas = 0;
+		int Numeros = 0;
+		for (int i = 0; i<contraseña.length(); i++) {
+			if(contraseña.substring(i, i+1).equalsIgnoreCase(contraseña.substring(i, i+1).toUpperCase())) {
+				Mayusculas = Mayusculas + 1;
+			}else {
+				if(contraseña.substring(i, i+1).equalsIgnoreCase(contraseña.substring(i, i+1).toLowerCase())){
+					Minusculas = Minusculas + 1;
+				}else {
+					Numeros = Numeros + 1;
+				}
+			}
+		}
+		if(Mayusculas>=2 && Minusculas>=1 && Numeros>=5) {
+			return true;
+		}else {
+			return false;
+		}
 	}
-	public String generarPassword() {
+	
+	public boolean esFuerte(String contraseña) {
+		int Mayusculas = 0;
+		int Minusculas = 0;
+		int Numeros = 0;
+		int valor;
+		for (int i = 0; i<contraseña.length(); i++) {
+			valor = (int) (contraseña.charAt(i));
+			if(valor>=48 && valor<=57) {
+				Numeros = Numeros + 1;
+			}else {
+				if(valor>=65 && valor<=90) {
+					Mayusculas = Mayusculas + 1;
+				}else {
+					if(valor>=97 && valor<=122) {
+						Minusculas = Minusculas + 1;
+					}
+				}
+			}
+		}
+		if(Mayusculas>=2 && Minusculas>=1 && Numeros>=5) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public void generarPassword(int longitud) {
 		Random r = new Random();
 		int num;
 		contraseña = "";
@@ -24,20 +69,28 @@ public class Password {
 			num = r.nextInt(3);
 			switch (num) {
 			case 0:
-				contraseña = contraseña + (char) r.nextInt(9) + 48;
+				contraseña = contraseña + (char) (r.nextInt(10) + 48);
+				
 				break;
 			case 1:
-				contraseña = contraseña + (char) r.nextInt(25) + 65;
+				contraseña = contraseña + (char) (r.nextInt(25) + 65);
+				
 				break;
 			case 2:
-				contraseña = contraseña + (char) r.nextInt(25) + 97;
+				contraseña = contraseña + (char) (r.nextInt(25) + 97);
+				
 				break;
 				}
 			}
-		return contraseña;
 	}
+	
 	public void setLongitud(int longitud) {
 		this.longitud = longitud;
 	}
-	
+	public int getLongitud() {
+		return this.longitud;
+	}
+	public String getContraseña() {
+		return this.contraseña;
+	}
 }
--- Password_Objetos/src/principal/Principal.java
+++ Password_Objetos/src/principal/Principal.java
@@ -1,5 +1,26 @@
 package principal;
 
-public class Principal {
+import java.util.Scanner;
 
+public class Principal {
+	
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		int tam;
+		
+		Password p = new Password();
+		System.out.println(p.esFuerte("ASDcvbn123456"));
+		
+		System.out.println("Dime el tamaño del vector");
+		tam = leer.nextInt();
+		Password[] password = new Password[tam];
+		System.out.println("Dime la longitud de las contraseñas");
+		int longitud;
+		leer = new Scanner(System.in);
+		longitud = leer.nextInt();
+		for(int i=0; i<tam; i++) {
+			password[i] =  new Password(longitud);
+		}
+		
+	}
 }
